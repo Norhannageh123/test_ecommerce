3a0c482c7c1218ab60f9d185b34a2b23f3c54be7
AhmedMHassaan <ahmed.m.hassaan1998@gmail.com>
2024-10-18 14:30:34 +0300

    using binding instead of findViewById (ViewBinding)

--- app/src/main/java/com/depi/myapplicatio/forget_password.kt
+++ app/src/main/java/com/depi/myapplicatio/forget_password.kt
@@ -12,6 +12,7 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.depi.myapplicatio.databinding.FragmentForgetPasswordBinding
 import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.Callback
@@ -25,38 +26,42 @@ import kotlin.random.Random
 
 class ForgetPasswordFragment : Fragment() {
 
-    private lateinit var inputEmail: EditText
-    private lateinit var btnSendCode: Button
-    private lateinit var btnBack: ImageButton
+
+    private lateinit var binding: FragmentForgetPasswordBinding
+
+//    private lateinit var inputEmail: EditText
+//    private lateinit var btnSendCode: Button
+//    private lateinit var btnBack: ImageButton
 
     // استخدم API Key الخاص بك هنا
-    private val mailgunApi: MailgunApi =
-        MailgunApi.create("e05467477a40b20ec6457fab5ceaa320-d010bdaf-d0596b60")
+//    private val mailgunApi: MailgunApi =
+//        MailgunApi.create("e05467477a40b20ec6457fab5ceaa320-d010bdaf-d0596b60")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // هنا نقوم فقط بإنشاء الـ View وإرجاعه
-        return inflater.inflate(R.layout.fragment_forget_password, container, false)
+        binding = FragmentForgetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // الآن نقوم بتعريف العناصر بعد إنشاء الـ View
-        inputEmail = view.findViewById(R.id.inputEmail)
+        /*inputEmail = view.findViewById(R.id.inputEmail)
         btnSendCode = view.findViewById(R.id.btnSendCode)
         btnBack = view.findViewById(R.id.btnBack)
-
+*/
         // عند الضغط على زر العودة
-        btnBack.setOnClickListener {
+        binding.btnBack.setOnClickListener {
             findNavController().navigate(R.id.action_forgetPassword_to_login)
         }
 
         // عند الضغط على زر "إرسال الكود"
-        btnSendCode.setOnClickListener {
-            val email = inputEmail.text.toString().trim()
+        binding.btnSendCode.setOnClickListener {
+            val email = binding.inputEmail.text.toString().trim()
             if (email.isNotEmpty()) {
                 sendVerificationEmail(email) // استدعاء دالة الإرسال
             } else {
